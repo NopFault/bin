405b950a15814478ef481caf5a724a990e9d9414
Nop Fault <nopfault@gmail.com>
2021-12-15 03:03:39 -0500

    Add wpu flag to web scrapper

--- web.sh
+++ web.sh
@@ -4,9 +4,14 @@ if [[ $# -gt 0 ]]; then
   action="$1"
 
   case $action in
-    -l|--links)
+      -l|--links)
       URL="$2"
       curl -f -L $URL | grep -Eo '"*://([a-zA-Z0-9#~.*,/!?=+&_%:-]*)"' | awk -F':\/\/' '{ print $2 }'
       ;;
+      --wpu)
+      URL="$2"
+      curl -f -L $URL/wp-json/wp/v2/users
+      ;;
     esac
+        
 fi
